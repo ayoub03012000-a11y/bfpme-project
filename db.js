541811a9f9e3f45111b9d641f90c123ad0ff895e
Ayoub <ayoub03012000@gmail.com>
2026-04-21 15:09:27 +0100

    fix mongo connection

--- db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb+srv://ayoub03012000_db_user:ssZzhsPLMJp9uBe@cluster0.bqzdn81.mongodb.net/bfpme?retryWrites=true&w=majority")
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.log("❌ MongoDB error:", err));
